874a9e33152bca139fd96682a7cb9c46d368b6ca
cameronyoung94 <68090343+cameronyoung94@users.noreply.github.com>
2021-03-01 12:36:44 -0500

    added routes-links-and switchs to the app component

--- secret-family-recipes-cookbook/src/App.js
+++ secret-family-recipes-cookbook/src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link, Switch } from "react-router-dom" 
+import Login from './components/login'
 
 function App() {
+
+  const logout = () => {
+
+  }
+
   return (
+  <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li>
+          <Link onClick={logout}>Logout</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route component={Login} />
+      </Switch>
     </div>
+  </Router>
   );
 }
 
